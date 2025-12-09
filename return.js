e71693ba89500c29733908c8bd3493c8eaa4f20e
Your Name <you@example.com>
2025-12-09 20:26:38 +0200

    correction

--- return.js
+++ return.js
@@ -1,7 +1,7 @@
 import {array} from "./list.js";
 
-function returnList(){
-    return [...array];
+function returnList(...l){
+    return [l,array];
 }
 
-console.log(returnList());
+console.log(returnList(3,4,5));
